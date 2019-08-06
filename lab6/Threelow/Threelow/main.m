580591be540ea4befce896fa4ebce28253678b52
Kohei Asai <me@axross.io>
2019-08-06 12:53:34 -0700

    Initialize project

---
+++ lab6/Threelow/Threelow/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  Threelow
+//
+//  Created by Kohei Asai on 2019-08-06.
+//  Copyright © 2019 Kohei Asai. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
